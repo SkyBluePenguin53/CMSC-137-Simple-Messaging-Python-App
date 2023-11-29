bd639e0e45dc44499f8945b8c6163eb46916f0d4
RB <rbjocsing@up.edu.ph>
2023-11-29 17:05:57 +0800

    Refactored everything, added asynchronous threads

--- Client.py
+++ Client.py
@@ -1,40 +1,55 @@
 import socket
+import asyncio
 from threading import Thread
+from aioconsole import ainput
 
-#   Get computer IP address
-host = socket.gethostname()
-#   Set gateway
-port = 1337
-separator_token = "<SEP>"
-
-
-#   initialize TCP socket
-s = socket.socket()
-print(f"[*] Connecting to {host}:{port}...")
-#   Connects to a server
-s.connect((host, port))
-print("[+] Connected.")
-
-name = input("Enter your preferred name: ")
-   
-def listen():
-    while True:
-        data = s.recv(1024).decode()
-        print("\n" + data)
+class Client():
+    def __init__(self) -> None:
+        #   Preamble
+        #   Initialize client parameters
+        self.host = socket.gethostname()
+        self.port = 1337
+        self.max_message_bits = 2048
+        self.separator_token = "<SEP>"
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print(f"[*] Connecting to {self.host}:{self.port}...")
+        self.server.connect((self.host, self.port))
+        print("[+] Connected.")
+        self.name = input("Enter your preferred name: ")
         
-t = Thread(target = listen)
-t.daemon = True
-t.start()
+        self._run_client()
+    
+    #   Helper method for listening to server broadcasts
+    async def _listen(self):
+        while True:
+            try:
+                data = await asyncio.to_thread(self.server.recv, self.max_message_bits)     #   Receive messages from server asynchronously
+                if not data:
+                    break
+                print("\n" + data.decode())
+            except ConnectionAbortedError:
+                print("Server disconnected. Closing client.")
+                self._close_client()
+                break
+    
+    #   Helper method to send messages to the server
+    async def _send(self):
+        while True:
+            to_send = await ainput()    #   Asynchronous equivalent to input()
+            if to_send.lower() == '[exit]':
+                self._close_client()
+                break
+            to_send = f"=> {self.name}{self.separator_token}{to_send}"
+            self.server.send(to_send.encode())
 
-while True:
-    # input message we want to send to the server
-    to_send =  input()
-    # a way to exit the program
-    if to_send.lower() == '[exit]':
-        break
-    to_send = f"=> {name}{separator_token}{to_send}"
-    # finally, send the message
-    s.send(to_send.encode())
+    #   Core method to run both listen and send at the same time asynchronously
+    def _run_client(self):
+        #   Use asynchronous looping to listen for and send messages at the same time
+        loop = asyncio.get_event_loop()
+        tasks = [self._listen(), self._send()]
+        loop.run_until_complete(asyncio.gather(*tasks))
 
-# close the socket
-s.close()
+    def _close_client(self):
+        self.server.close()
+
+instance = Client()
--- Server.py
+++ Server.py
@@ -1,54 +1,113 @@
 import socket
 from threading import Thread
 
-#   Get computer IP address
-host = socket.gethostname()
-#   Set gateway
-port = 1337
-separator_token = "<SEP>"
+class Server():
+    def __init__(self, max_load) -> None:
+        #   Preamble
+        #   Initialize server parameters
+        self.host = socket.gethostname()
+        self.port = 1337
+        self.separator_token = "<SEP>"
+        self.max_load = max_load
+        self.max_message_bits = 2048
+        self.client_sockets = set()
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)   #   Allows hot reloading of server, skip waiting for OS to release port
+        self.server.bind((self.host, self.port))                            #   Binds server to host and port, telling OS to start server 
+        self._exit_flag = False
+        self._run_server()
 
+    #   Helper method for listening to a connection
+    #   TODO: Handle [error] exiting socking from client gracefully
+    def _listen(self, conn):
+        while True:
+            try:
+                data = conn.recv(self.max_message_bits).decode()
+            except Exception as e:
+                print(f"[!] Error: {e}")
+                self.client_sockets.remove(conn)
+                print(self.client_sockets)
+            else:
+                self._broadcast(data, conn)
 
-#   Initialize list/set of all connected client's sockets
-client_sockets = set()
-#   Create TCP socket
-s = socket.socket()
-#   Reusable port
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-#   Looks for any incoming connections
-s.bind((host, port))
-#   Five clients, one server
-s.listen(5)
-
-print(f"[*] Listening as {host}:{port}")
-
-
-def listen(conn):
-    while True:
+    #   Helper function for sending messages to all sockets in active connections
+    def _broadcast(self, message, conn):
+        message = message.replace(self.separator_token, ": ")
+        for client in self.client_sockets:
+            try:
+                client.send(message.encode())
+            except Exception as e:
+                print(f"[!] Error: {e}")
+                self._remove(conn)
+    
+    #   Helper function for removing a socket from the active connections     
+    def _remove(self, conn):
         try:
-            data = conn.recv(1024).decode()
+            conn.close()
+        except:
+            pass    ##  TODO: add proper error handling if conn.close() unexpectedly fails
+        finally:
+            self.client_sockets.remove(conn)
+    
+    def _console_input(self):
+        while True:
+            command = input()
+            if command.lower() == 'exit':
+                print("Exiting server...")
+                self._exit_server()
+                break
+            else:
+                print(f"Unknown command: {command}")
+                
+    #   Core method, running server instance
+    def _run_server(self):
+        #   Listen to a maximum of max_load connections
+        self.server.listen(self.max_load)
+
+        print(f"[*] Listening as {self.host}:{self.port}")
+        
+        console_thread = Thread(target=self._console_input)
+        console_thread.daemon = True
+        console_thread.start()
+
+        try:
+            while not self._exit_flag:
+                # The accept() method of a Socket object stores two parameters
+                # connection -> socket object of the client
+                # address -> IP address of the client
+                client_socket, client_address = self.server.accept()
+
+                if self._exit_flag:
+                    break  # Exit the loop if the exit flag is set
+
+                print(f"[+] {client_address} connected.")
+
+                # Maintain a list of active connections to the server
+                self.client_sockets.add(client_socket)
+
+                # Spawn a new thread to listen to the requests of the new connection
+                client_thread = Thread(target=self._listen, args=(client_socket,))
+
+                # Daemon threads end whenever the main thread ends
+                client_thread.daemon = True
+
+                client_thread.start()
+
         except Exception as e:
             print(f"[!] Error: {e}")
-            client_sockets.remove(conn)
-        else:
-            data = data.replace(separator_token,": ")
-        for client_socket in client_sockets:
-            client_socket.send(data.encode())
 
-while True:
-    #   Keep listening for new connections always
-    client_socket, client_address = s.accept()
-    print(f"[+] {client_address} connected.")
-    # add the new connected client to connected sockets
-    client_sockets.add(client_socket)
-    # start a new thread that listens for each client's messages
-    t = Thread(target=listen, args=(client_socket,))
-    # make the thread daemon so it ends whenever the main thread ends
-    t.daemon = True
-    # start the thread
-    t.start()
+        # Call the exit method to close remaining connections and sockets
+        self._exit_server()
+    
+    ##  TODO: Call this method when exiting server frontend
+    ##  FIX: Handling closed sockets
+    def _exit_server(self):
+        self._exit_flag = True
+        #   Close all sockets in client_sockets
+        if len(self.client_sockets) > 0:
+            for conn in self.client_sockets:
+                conn.close()
+        # close server socket
+        self.server.close()
 
-# close client sockets
-for conn in client_sockets:
-    conn.close()
-# close server socket
-s.close()
+instance = Server(1000)
